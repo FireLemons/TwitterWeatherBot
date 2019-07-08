859d251f971d9413bfdbe1af2a9fd58111487f1b
FireLemons <shenyang64@gmail.com>
2019-07-08 15:15:03 -0500

    added getClosest function

--- util.js
+++ util.js
@@ -10,6 +10,45 @@ module.exports = {
         return ('0' + CST).substr(-2);
     },
     
+    //Gets the element from a sorted list closest to the given element. 
+    //  @param {object} elem The given object to be compared for similarity against
+    //  @param {object[]} list The sorted list to be searched for the most similar element
+    //  @param {function} compare The comparison function
+    //    @param {object} elem1 The first element to be compared
+    //    @param {object} elem2 The second element to be compared
+    //    @return {number} A negative value if elem1 comes before elem2 otherwise a positive value
+    //  @param {object} lastComparison Only used in recursive calls
+    //  @return {object} The most similar element in list to elem
+    getClosest(elem, list, compare, lastComparison = {"comparison": Infinity, "value": null}){
+        if(!list.length){
+            throw new Error('Cannot find closest element from empty list.');
+        }
+        
+        
+        let midpoint = Math.floor(list.length / 2), currentComparison = {
+            "comparison": compare(elem, list[midpoint]),
+            "value": list[midpoint]
+        }
+        
+        if(Math.abs(lastComparison.comparison) < Math.abs(currentComparison.comparison)){
+            return lastComparison.value;
+        }
+        
+        let newList;
+        
+        if(currentComparison.comparison < 0){
+            newList = list.slice(0, midpoint);
+        } else {
+            newList = list.slice(midpoint + 1, list.length);
+        }
+        
+        if(!newList.length){
+            return list[midpoint];
+        }
+        
+        return this.getClosest(elem, newList, compare, currentComparison);
+    },
+    
     //Picks a random element out of an array
     //  @param {array} arr An array to pick random elements from
     //  @return {object} The element from arr chosen at random
